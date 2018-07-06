828b151189c5bdc4fc14fc6d7ad3dfee9ca841a7
Jin <jili@heidrick.com>
2018-07-05 19:00:15 -0500

    ng-content: display contents inside the selector tag

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,15 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { ZippyComponent } from './zippy.component';
+import { WrapperComponent } from './wrapper.component';
+import { CounterComponent } from './counter.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    ZippyComponent
+    ZippyComponent,
+    WrapperComponent,
+    CounterComponent
   ],
   imports: [
     BrowserModule
